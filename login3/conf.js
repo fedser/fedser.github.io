6e51cd9feb897c1d6246d9fbbe10aaa3080c22a7
fedser87 <fedser87@gmail.com>
2016-09-27 16:00:57 +0400

    Login3

---
+++ login3/conf.js
@@ -0,0 +1,3 @@
+var androidStore = 'https://play.google.com/store/apps/details?id=com.fielmann.kls';
+var iosStore = 'https://itunes.apple.com/de/app/fielmann-app-kontaktlinsen/id1145363658?l=de&ls=1&mt=8';
+var webUrl = 'http://fielmann-kls-test.cloudapp.net';
