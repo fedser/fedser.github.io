ef775ecba3d2417ad16b9f78d76a92b13d626138
fedser87 <fedser87@gmail.com>
2016-09-27 11:41:16 +0400

    Added activate folder

---
+++ activate/conf.js
@@ -0,0 +1,3 @@
+var androidStore = 'https://play.google.com/store/apps/details?id=com.twitter.android';
+var iosStore = 'https://itunes.apple.com/il/app/twitter/id333903271?mt=8&message=';
+var webUrl = 'http://fielmann-kls-test.cloudapp.net';
