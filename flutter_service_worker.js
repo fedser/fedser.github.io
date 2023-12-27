b833d8e7dc926a7a1bebd07bca6ea3d932610b5e
fedser <fedser87@gmail.com>
2023-12-27 12:14:35 +0400

    Admin fixes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"version.json": "7af6ce9dc872617c9fe5afc16fd6f85e",
+const RESOURCES = {"version.json": "608c3ed4dbc2d39eac8e5b15c8c15cd0",
 "index.html": "05bd7b208a5b64f27287197b6f6e7257",
 "/": "05bd7b208a5b64f27287197b6f6e7257",
-"main.dart.js": "8715f0f314493f8201a0fefa5a5b63d2",
+"main.dart.js": "bd27500f32fc7201c334d61398d469bd",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
